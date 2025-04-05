25df307597b0dee89297b2f7bee43a16783012c3
masterthuku <peterthuku706@gmail.com>
2025-04-05 10:57:35 +0300

    onboarding fix

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,21 +1,26 @@
 import { getIndustryInsights } from "@/actions/dashboard";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
-import React from "react";
 import DashboardView from "./_components/DashboardView";
 
 const IndustryInsightsPage = async () => {
   const { isOnboarded } = await getUserOnboardingStatus();
-  const insights = await getIndustryInsights();
-
   if (!isOnboarded) {
     redirect("/onboarding");
   }
+
+  const insights = await getIndustryInsights();
+
+  // If insights is null, industry is missing, so redirect to onboarding
+  if (!insights) {
+    redirect("/onboarding");
+  }
+
   return (
     <div className="container mx-auto">
-      <DashboardView insights={insights}/>
+      <DashboardView insights={insights} />
     </div>
-  )
+  );
 };
 
 export default IndustryInsightsPage;
